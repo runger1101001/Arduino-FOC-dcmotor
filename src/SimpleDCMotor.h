cfa578a9747b158a309c23119317ce58f3f17c68
Richard Unger <richard.unger@nexum.com>
2023-01-09 00:05:25 +0100

    rename library

--- src/SimpleDCMotor.h
+++ src/SimpleDCMotor.h
@@ -5,5 +5,6 @@
 #include "drivers/DCDriver1PWM.h"
 #include "drivers/DCDriver2PWM.h"
 #include "drivers/DCDriver4PWM.h"
-#include "drivers/DCDriverSpeedDir.h"
+#include "drivers/DCDriver1PWM1Dir.h"
+#include "drivers/DCDriver1PWM2Dir.h"
 
